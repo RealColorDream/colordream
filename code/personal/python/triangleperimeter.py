3f9895d93571fa61202e970e3d296aee6c283806
ColorDream <notrealcolordream@gmail.com>
2022-02-05 23:14:41 +0100

    ajout du fichier triangleperimeter.py

---
+++ code/personal/python/triangleperimeter.py
@@ -0,0 +1,4 @@
+a=int(input('Whats the size of a side ? \n'))
+b=int(input('Whats the size of b side ? \n'))
+c=int(input('Whats the size of c side ? \n'))
+print('The triangle perimeter is : ' , a + b + c , 'cm')
